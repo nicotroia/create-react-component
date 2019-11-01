b9fcb7a9531cc6be4671e3bb3979f31f78e1901b
Nico Troia <nicotroia@gmail.com>
2019-10-31 23:59:07 -0400

    use createWithIntl helper

--- src/templates/ts/__tests__/Component.spec.tsx
+++ src/templates/ts/__tests__/Component.spec.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import render from 'react-test-renderer';
+import { createWithIntl, intl } from '@/misc/tests';
 
 import {{name}} from '../{{name}}';
 
+const mockProps = {
+  intl,
+};
+
 describe('{{name}} Tests', () => {
   it('should render without optional props', () => {
-    const component = render.create(<{{name}} />);
+    const component = createWithIntl(<{{name}} {...mockProps} />);
     expect(component).toMatchSnapshot();
   });
 });
